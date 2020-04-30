830e167f87834181d2cba29f9964be5db1b56b8a
Arrow <aronvinicius15@gmail.com>
2020-04-30 13:08:33 -0300

    Criando variáveis e renderizando elas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  // any[] = []; => []
+  // any[];      => undefined
+  // any é qualquer coisa / public nome_variavel: tipo_variavel
+
+  public todos: any [] = [];
+  public title: String = 'Minhas Tarefas';
+
+  constructor() {
+    this.todos.push('meu nome é aron');
+    this.todos.push('meu nome é vinicius');
+    this.todos.push('meu nome é custodio');
+    this.todos.push('meu nome é camossi');
+    this.todos.push('em');
+    this.todos.push(2001);
+    this.todos.push({ message: 'teste message' });
+    this.todos.push(new Date());  
+  }
 }
