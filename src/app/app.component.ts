99f395eea88ecae1337db62f69150d68a5421787
AVG1525 <aronvinicius15@gmail.com>
2020-05-28 13:35:26 -0300

    Adicianando tarefas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,17 +22,27 @@ export class AppComponent {
 
   constructor(private fb: FormBuilder) {    
     this.form = this.fb.group({
-      title: [, Validators.compose([
+      title: ['', Validators.compose([
         Validators.minLength(3),
         Validators.maxLength(60),
         Validators.required
       ])]
-    });    
-    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
-    this.todos.push(new Todo(2, 'Ir ao supermercado', false));
-    this.todos.push(new Todo(3, 'Cortar o cabelo', true));
+    });
   }
 
+  add(){
+    // this.form.value => { title: 'Titulo' }
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    
+    this.todos.push(new Todo(id, title, false));
+    this.clear();
+  }
+
+  clear(){
+    this.form.reset();
+  }
+  
   remove(todo: Todo){
     const index = this.todos.indexOf(todo);
     if(index !== -1){
